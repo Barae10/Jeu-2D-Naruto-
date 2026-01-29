8dbc96ab676f04893e237cbe431c3ced6e1bf651
Mansouri Bara'e <mansouribarae1@gmail.com>
2026-01-30 00:46:20 +0100

    Add files via upload

---
+++ FirstGame.py
@@ -0,0 +1,208 @@
+# Importation des modules nécessaires
+import pygame
+from sys import exit
+from random import randint, choice
+
+# Fonction pour charger le meilleur score
+def load_high_score():
+    try:
+        with open("high_score.txt", "r") as file:
+            return int(file.read())
+    except:
+        return 0
+
+# Fonction pour sauvegarder le meilleur score
+def save_high_score(score):
+    with open("high_score.txt", "w") as file:
+        file.write(str(score))
+
+# Définition de la classe du joueur
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        player_walk_1 = pygame.image.load('images/player1.png').convert_alpha()
+        player_walk_2 = pygame.image.load('images/player2.png').convert_alpha()
+        self.player_jump = pygame.image.load('images/player3.png').convert_alpha()
+        self.player_walk = [player_walk_1, player_walk_2]
+        self.player_index = 0
+        self.image = self.player_walk[self.player_index]
+        self.rect = self.image.get_rect(midbottom=(80, 390))
+        self.gravity = 0
+        self.jump_sound = pygame.mixer.Sound('audio/jump.mp3')
+        self.jump_sound.set_volume(0.5)
+
+    def player_input(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE] and self.rect.bottom >= 390:
+            self.gravity = -20
+            self.jump_sound.play()
+
+    def apply_gravity(self):
+        self.gravity += 1
+        self.rect.y += self.gravity
+        if self.rect.bottom >= 390:
+            self.rect.bottom = 390
+
+    def animation_state(self):
+        if self.rect.bottom < 390:
+            self.image = self.player_jump
+        else:
+            self.player_index += 0.1
+            if self.player_index >= len(self.player_walk):
+                self.player_index = 0
+            self.image = self.player_walk[int(self.player_index)]
+
+    def update(self):
+        self.player_input()
+        self.apply_gravity()
+        self.animation_state()
+
+class Obstacle(pygame.sprite.Sprite):
+    def __init__(self, type):
+        super().__init__()
+        if type == 'fly':
+            fly_1 = pygame.image.load('images/Kill0.png').convert_alpha()
+            fly_2 = pygame.image.load('images/Kill0.png').convert_alpha()
+            self.frames = [fly_1, fly_2]
+            y_pos = 300
+
+        elif type =='barae1':
+            barae1_1 = pygame.image.load('images/K0.png').convert_alpha()
+            barae1_2 = pygame.image.load('images/0K.png').convert_alpha()
+            self.frames = [barae1_1, barae1_2]
+            y_pos = 350
+
+        elif type =='barae2':
+            barae2_1 = pygame.image.load('images/Kill2.png').convert_alpha()
+            self.frames = [barae2_1]
+            y_pos = 390
+        
+        elif type =='barae3':
+            barae3_1= pygame.image.load('images/Kill1.png').convert_alpha()
+            barae3_2= pygame.image.load('images/Kill5.png').convert_alpha()
+            self.frames = [barae3_1,barae3_2]
+            y_pos= 350
+
+        else:
+            snail_1 = pygame.image.load('images/Kill3.png').convert_alpha()
+            snail_2 = pygame.image.load('images/Kill3.png').convert_alpha()
+            self.frames = [snail_1, snail_2]
+            y_pos = 390
+        self.animation_index = 0
+        self.image = self.frames[self.animation_index]
+        self.rect = self.image.get_rect(midbottom=(randint(900, 1100), y_pos))
+
+    def animation_state(self):
+        self.animation_index += 0.1
+        if self.animation_index >= len(self.frames):
+            self.animation_index = 0
+        self.image = self.frames[int(self.animation_index)]
+
+    def update(self):
+        self.animation_state()
+        self.rect.x -= 6
+        self.destroy()
+
+    def destroy(self):
+        if self.rect.x <= -100:
+            self.kill()
+
+def display_score():
+    current_time = int((pygame.time.get_ticks() / 1000)*3) - start_time
+    score_surf = test_font.render(f'Score: {current_time}', False, (64, 64, 64))
+    score_rect = score_surf.get_rect(center=(400, 50))
+    screen.blit(score_surf, score_rect)
+    return current_time
+
+def collision_sprite():
+    if pygame.sprite.spritecollide(player.sprite, obstacle_group, False):
+        obstacle_group.empty()
+        return False
+    else:
+        return True
+
+# Initialisation de Pygame
+pygame.init()
+screen = pygame.display.set_mode((800, 400))
+pygame.display.set_caption('Barae Game')
+clock = pygame.time.Clock()
+test_font = pygame.font.Font('Font/Pixeltype.ttf', 50)
+game_active = False
+start_time = 0
+score = 0
+high_score = load_high_score()
+
+player = pygame.sprite.GroupSingle()
+player.add(Player())
+
+obstacle_group = pygame.sprite.Group()
+
+sky_surface = pygame.image.load('images/Village.png').convert()
+ground_surface = pygame.image.load('images/sol.png').convert()
+
+player_stand = pygame.image.load('images/Naruto.png').convert_alpha()
+player_stand = pygame.transform.rotozoom(player_stand, 0, 2)
+player_stand_rect = player_stand.get_rect(center=(400, 220))
+
+game_name = test_font.render('Naruto Game', False, (111, 196, 169))
+game_name_rect = game_name.get_rect(center=(400, 80))
+game_message = test_font.render("Let's gooo", False, (111, 196, 169))
+game_message_rect = game_message.get_rect(center=(400, 330))
+
+obstacle_timer = pygame.USEREVENT + 1
+pygame.time.set_timer(obstacle_timer, 1500)
+pygame.mixer.music.load("audio/Naruto Shippuden - Opening 3  Blue Bird.mp3")
+pygame.mixer.music.play(-1)
+
+# Image d'arrière-plan au début du jeu (avant de commencer)
+intro_background = pygame.image.load('images/Intro.png').convert()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+        if game_active:
+            if event.type == obstacle_timer:
+                obstacle_group.add(Obstacle(choice(['fly','barae2', 'snail', 'barae1', 'snail','barae3'])))
+
+        else:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                game_active = True
+                start_time = int((pygame.time.get_ticks() / 1000)*3)
+
+    if game_active:
+        # Arrière-plan normal pendant le jeu
+        screen.blit(sky_surface, (0, 0))
+        screen.blit(ground_surface, (0, 320))
+        score = display_score()
+        player.draw(screen)
+        player.update()
+        obstacle_group.draw(screen)
+        obstacle_group.update()
+        game_active = collision_sprite()
+        
+    else:
+        # Arrière-plan d'introduction avant de commencer le jeu
+        screen.blit(intro_background, (0, 0))  # Afficher l'arrière-plan d'introduction ici
+        screen.blit(player_stand, player_stand_rect)
+        score_message = test_font.render(f'Your score: {score}', False, (111, 196, 169))
+        score_message_rect = score_message.get_rect(center=(400, 330))
+
+        if score > high_score:
+            high_score = score
+            save_high_score(high_score)
+
+        high_score_surf = test_font.render(f'Meilleur Score: {high_score}', False, (111, 196, 169))
+        high_score_rect = high_score_surf.get_rect(center=(400, 370))
+
+        screen.blit(game_name, game_name_rect)
+        if score == 0:
+            screen.blit(game_message, game_message_rect)
+        else:
+            screen.blit(score_message, score_message_rect)
+        screen.blit(high_score_surf, high_score_rect)
+
+    pygame.display.update()
+    clock.tick(60)
